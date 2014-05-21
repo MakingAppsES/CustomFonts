0d457343ef78a7a74467f6fa58f5fadc9cdee07e
antorof <antorof.dev@gmail.com>
2014-05-21 11:00:34 +0200

    Añadido el cambio de fuente en MainActivity.java Cambiado el layout activity_main.xml Cambiado el nombre del paquete Cambiado el icono

--- app/src/main/java/es/makingapps/customfonts/app/MainActivity.java
+++ app/src/main/java/es/makingapps/customfonts/app/MainActivity.java
@@ -1,9 +1,11 @@
-package es.makingapps.myapplication2.app;
+package es.makingapps.customfonts.app;
 
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,13 +14,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // text view
+        TextView txtCustomFont = (TextView) findViewById(R.id.tv_customfont);
+
+        // Font path
+        String fontPath = "fonts/FFF_Tusj.ttf";
+        // Loading Font Face
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+
+        // Applying font
+        txtCustomFont.setTypeface(tf);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
